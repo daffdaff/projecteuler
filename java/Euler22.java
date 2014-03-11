3a7b8480194c14718e7fd53277b1c7e00cdc7486
Daniel Dahlqvist <daniel.dahlqvist@cgi.com>
2014-03-11 10:53:44 +0100

    Project Euler #22

---
+++ java/Euler22.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+public class Euler22 {	
+	
+	public static void main(String[] args) {		
+		if (args.length == 0) {
+            System.out.println("Ange en fil! Kör med: \"java -jar Euler22.jar <fil.txt>\"");
+        }
+		else {		
+        	int total = 0;
+			StringBuilder sb = new StringBuilder();
+		
+			File file = new File(args[0]);
+			//File file = new File("/Uppdrag/Java/names1.txt");
+		  	try {			  	
+		  		Scanner scanner = new Scanner(file, "UTF-8");           
+	            while(scanner.hasNext()) {
+	               	sb.append(scanner.nextLine());
+	            }            
+	            scanner.close();
+		  	} catch (FileNotFoundException e) {
+	           	e.printStackTrace();
+	        }
+		
+		  	String[] array = sb.toString().replaceAll("\"", "").replaceAll(", ", ",").split(",");
+			Arrays.sort(array);
+		
+			for (String f : array) {
+				int score = 0;
+				for (int g = 0; g < f.length(); g++) {
+					switch (f.toUpperCase().charAt(g)) {
+						case 'Å':
+							score = score + 27;
+							break;						
+						case 'Ä':
+							score = score + 28;
+							break;						
+						case 'Ö':
+							score = score + 29;
+							break;
+						case ' ':
+							break;
+						default:
+							score = score + (f.toUpperCase().charAt(g) - 'A' + 1);
+							break;					
+					}					
+				}
+				score = score * (Arrays.asList(array).indexOf(f) + 1);
+				total += score;
+				System.out.println(Arrays.asList(array).indexOf(f) + 1 + ". " + f + " has " + score + " total is now at " + total);				
+			}		
+			System.out.println("------------------------");
+			System.out.println("Grand total score is: " + total);
+		}	
+	}
+}
