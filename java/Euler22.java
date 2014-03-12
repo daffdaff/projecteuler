892661e03cacb423bd9b69a47a8df6cf1be454cd
daffdaff <daffdaff@users.noreply.github.com>
2014-03-12 10:38:34 +0100

    Update Euler22.java

--- java/Euler22.java
+++ java/Euler22.java
@@ -1,6 +1,6 @@
 /* 
-This version is able to handle Swedish ÅÄÖ although the sorting for ÅÄÖ isn't working 100%, the score is "correct" based on that it sorts ÄÅÖ.
-It also handles a file with names listed as following "Åke Andersson, Örjan Öst, Erland Svensson" etc. compared to the original Euler that had only uppercase and no white space. The code solves Euler22 original file ok! Give a text file without BOM as arguemnt.
+* This version is able to handle Swedish ï¿½ï¿½ï¿½ although the sorting for ï¿½ï¿½ï¿½ isn't working 100%, the score is "correct" based on that it sorts ï¿½ï¿½ï¿½.
+* It also handles a file with names listed as following "ï¿½ke Andersson, ï¿½rjan ï¿½st, Erland Svensson" etc. compared to the original Euler that had only uppercase and no white space. The code solves Euler22 original file ok! Give a text file without BOM as arguemnt.
 */
 
 import java.io.File;
@@ -12,7 +12,7 @@ public class Euler22 {
 	
 	public static void main(String[] args) {		
 		if (args.length == 0) {
-            System.out.println("Ange en fil! Kör med: \"java -jar Euler22.jar <fil.txt>\"");
+            System.out.println("Ange en fil! Kï¿½r med: \"java -jar Euler22.jar <fil.txt>\"");
         }
 		else {		
         	int total = 0;
@@ -37,14 +37,16 @@ public class Euler22 {
 				int score = 0;
 				for (int g = 0; g < f.length(); g++) {
 					switch (f.toUpperCase().charAt(g)) {
-						case 'Å':
+						case 'ï¿½':
 							score = score + 27;
 							break;						
-						case 'Ä':
+						case 'ï¿½':
 							score = score + 28;
 							break;						
-						case 'Ö':
+						case 'ï¿½':
 							score = score + 29;
+							break;
+						case '-':
 							break;
 						case ' ':
 							break;
